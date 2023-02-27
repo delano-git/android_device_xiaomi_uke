eab09cc8cf6670c10309d02a56d8c5581502a28f
Woomymy <woomy@woomy.be>
2023-02-27 18:36:45 +0100

    muyu: Add vendor ramdisk and recovery dlkm configs
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I09cadc101b57511e9441b0ca7349121d2a5521d6
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,5 +16,12 @@ LOCAL_KERNEL := $(KERNEL_PATH)/kernel
 PRODUCT_COPY_FILES += \
 	$(LOCAL_KERNEL):kernel
 
+# Kernel modules
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules.load.vendor_ramdisk))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_PATH)/modules/, $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD))
+
+BOARD_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules.load.recovery))
+BOARD_RECOVERY_KERNEL_MODULES := $(addprefix $(KERNEL_PATH)/modules/, $(BOARD_RECOVERY_KERNEL_MODULES_LOAD))
+
 # Inherit the proprietary files
 include vendor/xiaomi/muyu/BoardConfigVendor.mk
