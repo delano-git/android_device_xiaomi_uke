6aa70ea5d603b16187dd46cb701f4d91e8fce8f9
dodyirawan85 <dev.irawans@gmail.com>
2026-02-16 05:53:45 +0700

    uke: Update from OS3.0.8.0.WOZMIXM
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- lineage_uke.mk
+++ lineage_uke.mk
@@ -23,5 +23,5 @@ PRODUCT_MODEL := 2410CRP4CG
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 16 BP2A.250605.031.A3 OS3.0.4.0.WOZMIXM release-keys" \
-    BuildFingerprint=Xiaomi/uke_global/uke:16/BP2A.250605.031.A3/OS3.0.4.0.WOZMIXM:user/release-keys
+    BuildDesc="missi-user 16 BP2A.250605.031.A3 OS3.0.8.0.WOZMIXM release-keys" \
+    BuildFingerprint=Xiaomi/uke_global/uke:16/BP2A.250605.031.A3/OS3.0.8.0.WOZMIXM:user/release-keys
