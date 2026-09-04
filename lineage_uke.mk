134d74ee720e4c495384add6cbdb3e8f9d2878d7
delano-git <noronha.delano@gmail.com>
2026-09-05 00:53:38 +0400

    uke: fix up fingerprint to avoid Xiaomi bloatware

--- lineage_uke.mk
+++ lineage_uke.mk
@@ -8,28 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-PRODUCT_GMS_CLIENTID_BASE := android-google
-
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/uke/device.mk)
 
-PRODUCT_DEVICE := uke
-PRODUCT_BRAND := Xiaomi
 PRODUCT_NAME := lineage_uke
+PRODUCT_DEVICE := uke
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2410CRP4CG
 
 PRODUCT_SYSTEM_NAME := uke_global
 PRODUCT_SYSTEM_DEVICE := uke
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 16 BP2A.250605.031.A3 16OS3.1.260625.171925424.QCPDGL.S release-keys" \
-    BuildFingerprint=Xiaomi/uke_global/uke:14/UKQ1.240624.001/OS3.0.303.0.WOZMIXM:user/release-keys \
+    BuildDesc="missi-user 16 BP2A.250625.000.00 OS3.0.303.0.WOZMIXM release-keys" \
+    BuildFingerprint=Xiaomi/uke_global/uke:16/BP2A.250625.000.00/OS3.0.303.0.WOZMIXM:user/release-keys
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Axion Device Configuration
 AXION_MAINTAINER := delano-git
